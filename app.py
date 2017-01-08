5ca24548b05aae164ee6ffc6c3ad4b89237e7094
eccentricayman <aahmed3@stuy.edu>
2017-01-08 03:42:38 -0500

    added basic app,py, homepage template, css, bootstrap, bootstrap notifications, app folder structure, and pictures

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#this one will have notifications and such via javascript
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+@app.route("/found")
+def found():
+	return render_template("found.html")
+
+@app.route("/lost")
+def lost():
+	return render_template("lost.html")
+
+if __name__ == "__main__":
+	#app.debug = True
+	app.run()
