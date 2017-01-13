12bc8d03e36cc4e656173d779535904d18e618af
Patrick Chan <patrickchan1999@gmail.com>
2017-01-13 11:58:47 -0500

    Refactored initialization of database in database.py

--- utils/database.py
+++ utils/database.py
@@ -2,34 +2,36 @@ import sqlite3
 
 class database:
 
-    def __init__(self):
+    def __init__(self, remove=False):
         self.db = sqlite3.connect("data/discobandit.db");
         self.cursor = self.db.cursor;
-        
-    def clone(self, remove=False):
         if remove:
-            dbCommand = "DELETE * FROM ListOfPetsFound;"
-            self.cursor.execute(dbCommand)
-            dbCommand = "DELETE * FROM ListofPetsLost;"
-            self.cursor.execute(dbCommand)
-            self.db.commit()
-        else:
-            dbCommand = "CREATE TABLE IF NOT EXISTS ListOfPetsFound (petID INTEGER, founderEmail TEXT);"
-            self.cursor.execute(dbCommand)
-            dbCommand = "CREATE TABLE IF NOT EXISTS ListOfPetsLost (petID INTEGER, ownerEmail TEXT);"
-            self.cursor.execute(dbCommand)
-            dbCommand = "CREATE TABLE IF NOT EXISTS Pets (petID INTEGER, location TEXT, petType TEXT, color TEXT, eyeColor TEXT, img TEXT, description TEXT, dateLost TEXT, petName TEXT);"
-            self.cursor.execute(dbCommand)
-            self.db.commit()
+            self.removeDatabase()
+        self.initDatabase()
 
+    def initDatabase(self):
+        dbCommand = "CREATE TABLE IF NOT EXISTS ListOfPetsFound (petID INTEGER, founderEmail TEXT);"
+        self.cursor.execute(dbCommand)
+        dbCommand = "CREATE TABLE IF NOT EXISTS ListOfPetsLost (petID INTEGER, ownerEmail TEXT);"
+        self.cursor.execute(dbCommand)
+        dbCommand = "CREATE TABLE IF NOT EXISTS Pets (petID INTEGER, location TEXT, petType TEXT, color TEXT, eyeColor TEXT, img TEXT, description TEXT, dateLost TEXT, petName TEXT);"
+        self.db.commit()
+        
+    def removeDatabase(self):
+        dbCommand = "DELETE * FROM ListOfPetsFound;"
+        self.cursor.execute(dbCommand)
+        dbCommand = "DELETE * FROM ListofPetsLost;"
+        self.cursor.execute(dbCommand)
+        self.db.commit()
+        
     def pullLostData(self, pullRequest=None):
         defaultCommand = "SELECT * FROM Pets, ListOfPetsLost"
         if pullRequest:
             dbCommand = defaultCommand + " " + pullRequest + ";"
         else:
             dbCommand = defaultCommand + ";"
-        self.cursor.execute(dbCommand)
-        data = self.cursor.fetchall()
+            self.cursor.execute(dbCommand)
+            data = self.cursor.fetchall()
         return data
 
     def pullFoundData(self, pullRequest=None):
@@ -38,36 +40,23 @@ class database:
             dbCommand = defaultCommand + " " + pullRequest + ";"
         else:
             dbCommand = defaultCommand + ";"
-        self.cursor.execute(dbCommand)
-        data = self.cursor.fetchall()
+            self.cursor.execute(dbCommand)
+            data = self.cursor.fetchall()
         return data
-        
-    def pull(self, flag):
-        if( flag == "lost"):
-            dbCommand = "SELECT * FROM Pets, ListOfPetsLost WHERE Pets.petID = ListOfPetsLost.petID;"
-            self.cursor.execute(dbCommand)
-            data = self.cursor.fetchall()
-            return data
-        elif( flag == "found"):
-            dbCommand = "SELECT * FROM Pets, ListOfPetsFound WHERE Pets.petID = ListOfPetsFound.petID;"
-            self.cursor.execute(dbCommand)
-            data = self.cursor.fetchall()
-            return data
-        else:
-            print("Flag not found")
 
-    def add( self, petData, userInfo): #petData is a dictionary
-        #Assume that all fields are filled in, with "" if necessary
-        #Assume petID is generated beforehand
-        dbCommand = "INSERT INTO Pets VALUES (%d, %s, %s, %s, %s, %s, %s, %s, %s)"%(petData['petID'], petData['location'], petData['petType'], petData['color'], petData['eyeColor'], petData['img'], petData['description'], petData['dateLost'], petData['petName'])
-        self.cursor.execute(dbCommand)
-        if( 'ownerEmail' in userInfo):
-            dbCommand = "INSERT INTO ListOfPetsLost VALUES (%d, %s)"%(petData['petID'], userInfo['ownerEmail'])
-            self.cursor.execute(dbCommand)
-        elif( 'founderEmail' in userInfo):
-            dbCommand = "INSERT INTO ListOfPetsFound VALUES (%d, %s)"%(petData['petID'], userInfo['foundEmail'])
-            self.cursor.execute(dbCommand)
-        self.db.commit()
+ def add( self, petData, userInfo):
+     #petData is a dictionary
+     #Assume that all fields are filled in, with "" if necessary
+     #Assume petID is generated beforehand
+     dbCommand = "INSERT INTO Pets VALUES (%d, %s, %s, %s, %s, %s, %s, %s, %s)"%(petData['petID'], petData['location'], petData['petType'], petData['color'], petData['eyeColor'], petData['img'], petData['description'], petData['dateLost'], petData['petName'])
+     self.cursor.execute(dbCommand)
+     if 'ownerEmail' in userInfo:
+         dbCommand = "INSERT INTO ListOfPetsLost VALUES (%d, %s)"%(petData['petID'], userInfo['ownerEmail'])
+         self.cursor.execute(dbCommand)
+     elif 'founderEmail' in userInfo:
+         dbCommand = "INSERT INTO ListOfPetsFound VALUES (%d, %s)"%(petData['petID'], userInfo['foundEmail'])
+         self.cursor.execute(dbCommand)
+         self.db.commit()
 
     def remove( self, petID ):
         dbCommand = "DELETE * FROM Pets, ListOfPetsLost, ListOfPetsFound WHERE petID = %d" % (petID)
