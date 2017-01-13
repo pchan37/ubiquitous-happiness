de72da01fab5ec324e3bbaf2319fdb932af72723
Patrick Chan <patrickchan1999@gmail.com>
2017-01-13 12:11:50 -0500

    Added database component into app.py

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
 from flask import Flask, render_template
+from utils import database
 
 app = Flask(__name__)
+db = new database()
 
 #this one will have notifications and such via javascript
 @app.route("/")
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
 
 @app.route("/found/")
 def found():
-	return render_template("found.html")
+    return render_template("found.html")
 
 @app.route("/lost/")
 def lost():
-	return render_template("lost.html")
+    return render_template("lost.html")
 
 @app.route("/pet/<petID>")
-def petInfo( petID ):
-        data = pullFound("WHERE Pets.petID = %d AND Pets.petID = ListOfPetsFound.petID"%(petID));
-        if ( not any(data) ):
-                data = pullLost("WHERE Pets.petID = %d AND Pets.petID = ListOfPetsLost.petID"%(petID))
-        return render_template("pet.html",  location = data['location'], petType = data['petType'], color = data['color'], eyeColor = data['eyeColor'], img = data['img'], description = data['description'], dateLost = data['dateLost'], petName = data['petName'] )
+def petInfo(petID):
+    data = db.pullFoundData("WHERE Pets.petID = %d AND Pets.petID = ListOfPetsFound.petID"%(petID));
+    if (not any(data) ):
+            data = db.pullLostData("WHERE Pets.petID = %d AND Pets.petID = ListOfPetsLost.petID"%(petID))
+    return render_template("pet.html",  location = data['location'], petType = data['petType'], color = data['color'], eyeColor = data['eyeColor'], img = data['img'], description = data['description'], dateLost = data['dateLost'], petName = data['petName'] )
 
 
 @app.route("/petTest/")
 def petTest( ):
-        return render_tempate("pet.html")
-        
+    return render_tempate("pet.html")
+
 if __name__ == "__main__":
-        app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
