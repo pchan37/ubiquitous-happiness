82541e776db707bf96c0356e35206c617c0ae822
Kevin Zhang <kevinzhang3702@gmail.com>
2017-01-14 19:47:30 -0500

    something

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from utils.Database import Database
 
 app = Flask(__name__)
@@ -27,9 +27,29 @@ def petInfo(petID):
         return render_template("pet.html",  location = data['location'], petType = data['petType'], color = data['color'], eyeColor = data['eyeColor'], img = data['img'], description = data['description'], dateLost = data['dateLost'], petName = data['petName'] )
     else:
         return "petID not on record"
-@app.route("/petTest/")
-def petTest( ):
-    return render_tempate("pet.html")
+
+@app.route("/updateFound/")
+def updateFound():
+    formData = request.args();
+    string = "WHERE Pets.petID = ListOfPetsFound.petID"
+    if('location' in formData and formData['location'] != "" ):
+        string += " AND Pets.location = %s OR Pets.location != ''"%(formData['location'].lower());
+    if('petType' in formData and formData['petType'] != "" ):
+        string += " AND Pets.petType = %s OR Pets.petType != ''"%(formData['petType'].lower());
+    if('color' in formData and formData['color'] != "" ):
+        string += " AND Pets.color = %s OR Pets.color != ''"%(formData['color'].lower());
+    if('eyeColor' in formData and formData['eyeColor'] != "" ):
+        string += " AND Pets.eyeColor = %s OR Pets.eyeColor != ''"%(formData['eyeColor'].lower());
+    #if('img' in formData and formData['img'] != "" ):
+    #    string += " AND Pets.img = %s OR Pets.img != ''"%(formData['location']);
+    #if('description' in formData and formData['description'] != "" ):
+    #    string += " AND Pets.location = %s OR Pets.location != ''"%(formData['location']);
+    #if('dateLost' in formData and formData['dateLost'] != "" ):
+    #    string += " AND Pets.location = %s OR Pets.location != ''"%(formData['location']);
+    if('petName' in formData and formData['petName'] != "" ):
+        string += " AND Pets.location = %s OR Pets.location != ''"%(formData['petName'].lower());
+    db = Database()
+    
 
 if __name__ == "__main__":
     app.debug = True
